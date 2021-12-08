75be858449a9bc79b85d3e660a8c4ea7f8bdbe86
Alvin Galit <aroblesgalit@gmail.com>
2021-12-07 23:38:53 -0700

    Styled nav

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #949eaa;
+  background-color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 100px;
+  background-color: #202a35;
+}
+
+#nav img {
+  max-width: 30px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #949eaa;
 }
 
 #nav a.router-link-exact-active {
