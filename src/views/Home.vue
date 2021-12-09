2f72b2621aa399cbf220fe024fd7878f7364f90a
Alvin Galit <aroblesgalit@gmail.com>
2021-12-08 23:45:54 -0700

    Added one game

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="home">
-    Free to Play Games
+    <h2>Featured</h2>
+    <div class="grid">
+      <div class="box">
+        <div class="images">
+          <img
+            src="https://www.freetogame.com/g/1/dauntless-1.jpg"
+            alt="screenshot"
+          />
+          <!-- <img
+            src="https://www.freetogame.com/g/1/dauntless-2.jpg"
+            alt="screenshot"
+          />
+          <img
+            src="https://www.freetogame.com/g/1/dauntless-3.jpg"
+            alt="screenshot"
+          />
+          <img
+            src="https://www.freetogame.com/g/1/dauntless-4.jpg"
+            alt="screenshot"
+          /> -->
+        </div>
+        <div class="body">
+          <h3>Dauntless</h3>
+          <p>
+            A free-to-play, co-op action RPG with gameplay similar to Monster
+            Hunter.
+          </p>
+          <div class="details">
+            <span>MMORPG</span>
+            <span>Windows</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,3 +45,13 @@ export default {
   components: {}
 }
 </script>
+
+<style scoped>
+.home {
+  padding: 40px 100px;
+}
+
+h2 {
+  text-align: left;
+}
+</style>
