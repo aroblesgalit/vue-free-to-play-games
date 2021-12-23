571fc19e7801ca5c5c168e1a22fe77207d3a829d
Alvin Galit <aroblesgalit@gmail.com>
2021-12-23 00:21:32 -0700

    Used the name params to update title for the category view

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="category">
-    <h1>Category Title</h1>
+    <h1>{{ name.toUpperCase() }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Category'
+  name: 'Category',
+  data () {
+    return {
+      name: ''
+    }
+  },
+  created () {
+    console.log(this.$route)
+    this.name = this.$route.params.name
+  }
 }
 </script>
 
