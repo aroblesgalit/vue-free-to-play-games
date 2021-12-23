a5c0a92f6972e174f5b71acbf50bd477795b02be
Alvin Galit <aroblesgalit@gmail.com>
2021-12-23 00:05:24 -0700

    Updated category path

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '?category=:name',
+    path: '/category/:name',
     name: 'Category',
     component: Category
   }
