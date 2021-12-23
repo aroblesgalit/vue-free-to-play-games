e6d0da4d62ac96bf4a4bd5e170520a42b3525020
Alvin Galit <aroblesgalit@gmail.com>
2021-12-22 23:45:14 -0700

    Created a view page for viewing all games from a specific category

---
+++ src/views/Category.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="category">
+    <h1>Category Title</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category'
+}
+</script>
+
+<style scoped>
+#category {
+  padding: 40px 100px;
+}
+</style>
